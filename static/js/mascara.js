6f5d5e333349ce964b9ac0e622bf94263daa5fac
williams <williams.lima989@gmail.com>
2024-02-22 23:43:32 -0300

    Correção na mascara dos templates

--- static/js/mascara.js
+++ static/js/mascara.js
@@ -1,15 +1,15 @@
 function criaMascara(mascaraInput) {
-    const maximoInput = document.getElementById(`${mascaraInput}Input`).maxLength;
-    let valorInput = document.getElementById(`${mascaraInput}Input`).value;
-    let valorSemPonto = document.getElementById(`${mascaraInput}Input`).value.replace(/([^0-9])+/g, "");
+    const maximoInput = document.getElementById(`${mascaraInput}`).maxLength;
+    let valorInput = document.getElementById(`${mascaraInput}`).value;
+    let valorSemPonto = document.getElementById(`${mascaraInput}`).value.replace(/([^0-9])+/g, "");
     const mascaras = {
-        CPF: valorInput.replace(/[^\d]/g, "").replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"),
-        Celular: valorInput.replace(/[^\d]/g, "").replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3"),
-        CEP: valorInput.replace(/[^\d]/g, "").replace(/(\d{5})(\d{3})/, "$1-$2"),
-        RG: valorInput.replace(/[^\d]/g, ""),
-        CNJ: valorInput.replace(/[^\d]/g, "").replace(/(\d{7})(\d{2})(\d{4})(\d{1})(\d{2})(\d{4})/, "$1-$2.$3.$4.$5.$6"),
+        cpf: valorInput.replace(/[^\d]/g, "").replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"),
+        telefone: valorInput.replace(/[^\d]/g, "").replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3"),
+        cep: valorInput.replace(/[^\d]/g, "").replace(/(\d{5})(\d{3})/, "$1-$2"),
+        rg: valorInput.replace(/[^\d]/g, ""),
+        cnpj: valorInput.replace(/[^\d]/g, "").replace(/(\d{7})(\d{2})(\d{4})(\d{1})(\d{2})(\d{4})/, "$1-$2.$3.$4.$5.$6"),
     };
 
-    valorInput.length === maximoInput ? document.getElementById(`${mascaraInput}Input`).value = mascaras[mascaraInput]
-        : document.getElementById(`${mascaraInput}Input`).value = valorSemPonto;
+    valorInput.length === maximoInput ? document.getElementById(`${mascaraInput}`).value = mascaras[mascaraInput]
+        : document.getElementById(`${mascaraInput}`).value = valorSemPonto;
 };
